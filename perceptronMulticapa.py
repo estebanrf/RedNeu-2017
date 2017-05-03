c8336a9b608fb94b9c1dd2c9d3196ea08211fa37
Esteban Fernandez <esteban.pmf@gmail.com>
2017-05-02 22:39:05 -0300

    perceptronMulticapa finalizado.

--- perceptronMulticapa.py
+++ perceptronMulticapa.py
@@ -7,108 +7,114 @@ from Parser import parse_ej2
 
 
 class TrainingSpecs(object):
-	
-	def __init__(self, activation_fx, activation_fx_derivative, eta, epochs, epsilon, must_train_in_training_set_order):     
 
-		self.activation_fx = activation_fx
-		self.activation_fx_derivative = activation_fx_derivative
+	def __init__(self, eta, epochs, epsilon, must_train_in_training_set_order):
+
 		self.eta = eta
 		self.epochs = epochs
 		self.epsilon = epsilon
 		self.must_train_in_training_set_order = must_train_in_training_set_order
 
-class HiddenLayerSpecs(object):
+class Layer(object):
 
-	def __init__(self, perceptrons_count, perceptron_input_size):
+	def __init__(self, layer_input_size, neurons_count, activation_fx,
+				 activation_fx_derivative, use_uniform_distr_to_init_W):
 
-		self.perceptrons_count = perceptrons_count
-		self.perceptrons_input_size = perceptron_input_size
+		self.layer_input_size = layer_input_size
+		self.neurons_count = neurons_count
+		self.activation_fx = activation_fx
+		self.activation_fx_derivative = activation_fx_derivative
+		if use_uniform_distr_to_init_W:
+			self.neurons_matrix = np.random.uniform(0.0, 1.0, (layer_input_size, neurons_count))
+		else:
+			self.neurons_matrix = np.random.rand((layer_input_size, neurons_count))
 
 class PerceptronMulticapa(object):
 
-		def __init__(self, pattern_size, use_uniform_distr_to_init_W, hidden_layers_specs, output_size):     
-	
-			are_consistent_layers_specs(pattern_size, hidden_layers_specs)
+		def __init__(self, pattern_size, hidden_layers, output_layer):
+
+			# no creo que sea necesario lo del pattern_size, me parece que
+			# estaria mejor hacer un pre chequeo de nuestro set de entrenamiento
+			# y si vemos que no coincide con la primer capa oculta ahi si que chotee.
+
+			# are_consistent_layers_specs(pattern_size, hidden_layers_specs)
 
 			self.pattern_size = pattern_size
-			
-			self.hidden_layers_Ws = [ [self.init_neuron_w(layer_specs.perceptrons_input_size, use_uniform_distr_to_init_W) for _ in range(0,layer_specs.perceptrons_count) ] for layer_specs in hidden_layers_specs]
-
-			last_hidden_layer_specs = hidden_layers_specs[-1] 
-			self.output_layer_W = [self.init_neuron_w(1 + last_hidden_layer_specs.perceptrons_count, use_uniform_distr_to_init_W) for _ in range(0, output_size)]
-		
-		def init_neuron_w(self, weights_size, use_uniform_distr_to_init_W):
-			#No pude setear la varianza que se recomendo en la clase, no se si cambia en algo.
-			if use_uniform_distr_to_init_W:
-				return np.random.uniform(0.0, 1.0, weights_size)
-			return np.random.rand(weights_size)
+			self.hidden_layers = hidden_layers
+			self.output_layer = output_layer
 
 		def train(self, X, Y, training_specs):
 
 			self.eta = training_specs.eta
 			self.epochs = training_specs.epochs
-			
 			training_set = zip(X, Y)
-			
+
 			training_error_by_epoch = []
 
 			for _ in range(self.epochs):
 				epoch_error = 0
 
-				if training_specs.must_train_in_training_set_order:
+				if not training_specs.must_train_in_training_set_order:
 					shuffle(training_set)
 
-				for x, expected in training_set:  
-					V = self.calculate_V(x, training_specs.activation_fx)
-					self.back_propagation(V, expected, training_specs.activation_fx_derivative)
-					epoch_error += (np.linalg.norm(np.subtract(V[-1], expected)) / 2)
+				for x, expected in training_set:
+					V = self.calculate_V(x)
+					self.back_propagation(V, expected)
+					#epoch_error += (np.linalg.norm(np.subtract(V[-1], expected)) / 2)
+
+				epoch_error = self.validate(X,Y)
 				
-				training_error_by_epoch.append(epoch_error / len(training_set))
+				training_error_by_epoch.append(epoch_error)
 				#Error de una epoca = promedio del error (no se si esta bien)
-				if (epoch_error / len(training_set)) <= training_specs.epsilon:
+				if (epoch_error / float(len(training_set))) <= training_specs.epsilon:
 					break
 
 			return training_error_by_epoch
 
-		def validate(self, X, Y, activation_fx):
-			error = 0 
+		def validate(self, X, Y):
+			error = 0
 			for x, expected in zip(X,Y):
-				V = self.calculate_V(x, activation_fx)
-				error += (np.linalg.norm(np.subtract(V[-1], expected)) / 2)
-			return error
+				V = self.calculate_V(x)
 
-		def calculate_V(self, x, activation_function):
-			
+				error += (np.linalg.norm(np.subtract(V[-1], expected)) / 2)
+
+			return error / float(len(X))
+
+		def calculate_V(self, x):
+
 			V = []
 			V_i = x
-			i = 0
-			for current_layer_W in self.hidden_layers_Ws:
-				V_i_plus_1 = activation_function(np.dot(V_i, np.transpose(current_layer_W)))
-				V.append(V_i)
-				V_i = (np.insert(V_i_plus_1,0,-1))
 
-			V_M = activation_function(np.dot(V_i, np.transpose(self.output_layer_W)))
+			# en V guardo el patron de entrenamiento y las salidas de las capas
+			for current_layer in self.hidden_layers:
+				V.append(V_i)
+				V_i = current_layer.activation_fx(np.dot(V_i, current_layer.neurons_matrix))
+				V_i = np.insert(V_i,0,-1)
+
 			V.append(V_i)
+			V_M = self.output_layer.activation_fx(np.dot(V_i, self.output_layer.neurons_matrix))
 			V.append(V_M)
 			return V
-		
 
-		def back_propagation(self, V, expected, activation_fx_derivative):
+		def back_propagation(self, V, expected):
 
 			V_M = V[-1]
 			V_M_minus_1 = V[-2]
 			error  = np.subtract(expected,V_M)
-			current_deltas = np.multiply(activation_fx_derivative(np.dot(V_M_minus_1, np.transpose(self.output_layer_W))), error)
+			current_delta = np.multiply(self.output_layer.activation_fx_derivative(np.dot(V_M_minus_1, self.output_layer.neurons_matrix)), error)
+			propagation = np.dot(current_delta, np.transpose(self.output_layer.neurons_matrix[1:]))
+			self.output_layer.neurons_matrix += self.eta * np.outer(V_M_minus_1, current_delta)
+			V_index = -2
 
-			propagation = np.dot(np.transpose(self.output_layer_W)[1:], current_deltas) 
-			self.output_layer_W += self.eta * np.outer(current_deltas, V_M_minus_1)		
-
-			for hidden_layer_index, V_i_minus_1 in enumerate(reversed(V[:-2])):
-					#Iteramos las capas ocultas en reversa tambien, esa es la razon del calculo del indice
-					hidden_layer_index = (hidden_layer_index + 1) * -1
-					current_deltas = np.multiply(activation_fx_derivative(np.dot(V_i_minus_1, np.transpose(self.hidden_layers_Ws[hidden_layer_index]))), propagation)
-					propagation = np.dot(np.transpose(self.hidden_layers_Ws[hidden_layer_index])[1:], current_deltas)
-					self.hidden_layers_Ws[hidden_layer_index] += (self.eta * np.outer(current_deltas, V_i_minus_1))
+			for hidden_layer in reversed(self.hidden_layers):
+				#Iteramos las capas ocultas en reversa tambien, esa es la razon del calculo del indice
+				hidden_layer_index = -1
+				# como ya propagamos por la capa de salida, ahora empezamos desde el -2 en V
+				V_i_minus_1 = V[V_index - 1]
+				current_delta = np.multiply(hidden_layer.activation_fx_derivative(np.dot(V_i_minus_1, hidden_layer.neurons_matrix)), propagation)
+				propagation = np.dot(current_delta, np.transpose(hidden_layer.neurons_matrix[1:]))
+				hidden_layer.neurons_matrix += self.eta * np.outer(V_i_minus_1, current_delta)
+				V_index = V_index - 1
 
 #Funciones de activacion y sus derivadas, agregar mas. ----------------------------------------------------------------
 def ReLU(x):
@@ -123,14 +129,14 @@ def tanh_deriv(x):
     return (1 - tanh(x)**2)
 
 def binary_sigmoidal(x):
-	return 1.0 / (1 + np.exp(-1 * x))
+	return 1.0 / (1 + np.exp(-2 * x))
 
 def binary_sigmoidal_derivative(x):
-	return binary_sigmoidal(x) * (1 - binary_sigmoidal(x))
+	return 2 * binary_sigmoidal(x) * (1 - binary_sigmoidal(x))
 #----------------------------------------------------------------------------------------------------------------------
 
 def are_consistent_layers_specs(pattern_size, hidden_layers_specs):
-	
+
 	if len(hidden_layers_specs) == 0:
 		raise ValueError("There must be at least one Hidden Layer.")
 
@@ -138,46 +144,49 @@ def are_consistent_layers_specs(pattern_size, hidden_layers_specs):
 		raise ValueError("Pattern Size must be equals to First Hidden Layer Perceptron's Input Size.")
 
 	curr = 0
-	
+
 	while curr != len(hidden_layers_specs) - 1:
 		nextl = curr + 1
-	
+
 		if hidden_layers_specs[curr].perceptrons_count + 1 != hidden_layers_specs[nextl].perceptrons_input_size:
 			raise ValueError("Layers specifications are inconsistent.")
 
 		curr = nextl
-
 #----------------------------------------------------------------------------------------------------------------------
 
-training_specs = TrainingSpecs(binary_sigmoidal, binary_sigmoidal_derivative, 0.1, 10, 0.25, True)
+training_specs = TrainingSpecs(0.001, 600, 0.01, True)
 training_error_by_epoch = []
 validation_error = -1
 X = []
 Y = []
 
-#para testear localmente XOR o con el ejercicio 2 
+#para testear localmente paridad o con el ejercicio 2
 if False:
 	#Lo necesario para el XOR.
-	X = [(-1,0,0), (-1,0,1), (-1,1,0) , (-1, 1,1)]
-	Y = [[0],[1],[1],[0]]
+	X = [[-1,0,0,0], [-1,0,1,0], [-1,1,0,0] , [-1, 1,1,0], [-1,0,0,1], [-1,0,1,1], [-1,1,0,1] , [-1, 1,1,1]]
+	Y = [[0],[1],[1],[0],[1],[0],[0],[1]]
 else:
 	X, Y = parse_ej2()
+	
 	X = map(lambda x: np.insert(x, 0, -1), X)
 
 
 
-#Inicializamos perceptron, entrenamos y validamos.
-hidden_layer_specs = [HiddenLayerSpecs(4, len(X[0]))]
-ppm = PerceptronMulticapa(len(X[0]), True, hidden_layer_specs, len(Y[0]))
-training_error_by_epoch = ppm.train(X, Y, training_specs)
-validation_error = ppm.validate(X, Y, training_specs.activation_fx)
+#Inicializamos perceptron,
+hidden_layers = [Layer(len(X[0]), 20, binary_sigmoidal, binary_sigmoidal_derivative, True)]
+output_layer = Layer(21, 2, lambda x: x,  lambda x: 1, True)
+ppm = PerceptronMulticapa(len(X[0]), hidden_layers, output_layer)
 
-#Plot de error de entrenamiento
+#Entrenamos y validamos.
+training_error_by_epoch = ppm.train(X[:20], Y[:20], training_specs)
+validation_error = ppm.validate(X[:20], Y[:20])
+
+# Plot de error de entrenamiento
 plt.plot(range(1, len(training_error_by_epoch)+1), training_error_by_epoch, marker='o')
 plt.xlabel('Epoch')
 plt.ylabel('Epoch Error')
 plt.show()
 
-#Ya veremos como printear el error de validacion.
+# Ya veremos como printear el error de validacion.
 print("VALIDATION ERROR:")
-print(validation_error / len(X))
+print(validation_error)
