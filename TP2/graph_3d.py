975f4bff8b7297d52b62ddde824959f4f5cc47ca
Muchinik Francisco <fmuchinik@arsat.com.ar>
2017-06-23 14:53:23 -0300

    cambios a hebbian

--- TP2/graph_3d.py
+++ TP2/graph_3d.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot(training, validation):
-	#los parametros de entrada deben ser [(x1,y1,z1,categoria1), ... , (xn, yn, zn,categoria9)]
+def plot(training, validation=[]):
+	#los parametros de entrada deben ser [(categoria1,x1,y1,z1), ... , (categorian,xn, yn, zn)]
 
 	colors=['b', 'g', 'r', 'pink', 'y', 'm', 'w', 'k', 'cyan']
 
@@ -13,8 +13,10 @@ def plot(training, validation):
 
 
 	for points, m in [(training, 'o'), (validation, '^')]:
-		for x, y, z, cat in points:
-		    ax.scatter(x, y, z, c=colors[cat-1], marker=m)
+		for cat, x, y, z in points:
+			if isinstance(cat, int) or cat.is_integer():
+				cat = int(cat)
+			ax.scatter(x, y, z, c=colors[cat-1], marker=m)
 
 	ax.set_xlabel('X')
 	ax.set_ylabel('Y')
@@ -23,4 +25,4 @@ def plot(training, validation):
 	plt.show()
 
 
-plot([(1,1,1,6),(1,2,3,2)], [(2,3,4,4), (3,4,5,9)])
+#plot([(1,1.2,1.5,6.3),(1,2,3,2)], [(2,3,4,4), (3,4,5,9)])
